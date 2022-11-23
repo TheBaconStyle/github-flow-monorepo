6e4341d86b2a8aa62eedcbfb23c83f92c24bcd82
TheBaconStyle <maks.christmas2001@gmail.com>
2022-11-24 00:45:57 +0200

    feat(nx.json): added frontend client app
    * added NEXT.JS app
    * added tsconfig.base.json in project

---
+++ packages/frontend/client/next.config.js
@@ -0,0 +1,11 @@
+//@ts-check
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withNx } = require('@nrwl/next/plugins/with-nx');
+
+/**
+ * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = { nx: { svgr: false } };
+
+module.exports = withNx(nextConfig);
